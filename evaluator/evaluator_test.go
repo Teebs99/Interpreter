9ccb5b875c51b0aeff386f785755f8b6da13984b
Stephen Ives <teebs99@gmail.com>
2023-07-25 22:43:38 -0400

    add support for string concatenation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -219,6 +219,10 @@ func TestErrorHandling(t *testing.T) {
 			"identifier not found: foobar",
 		},
 		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
+		{
 			`
 if (10 > 1) {
   if (10 > 1) {
@@ -309,15 +313,29 @@ func TestFunctionApplication(t *testing.T) {
 }
 
 func TestStringLiteral(t *testing.T) {
-    input := `"Hello World!"`
+	input := `"Hello World!"`
 
-    evaluated := testEval(input)
-    str, ok := evaluated.(*object.String)
-    if !ok {
-        t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
-    }
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
 
-    if str.Value != "Hello World!" {
-        t.Errorf("String has wrong value. got=%q", str.Value)
-    }
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
 }
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -8,27 +8,27 @@ import (
 
 func TestNextToken(t *testing.T) {
 	input := `let five = 5;
-		let ten = 10;
+let ten = 10;
 
-		let add = fn(x, y) {
-		x + y;
-		};
+let add = fn(x, y) {
+  x + y;
+};
 
-		let result = add(five, ten);
-		!-/*5;
-		5 < 10 > 5;
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
 
-		if (5 < 10) {
-			return true;
-		} else {
-			return false;
-		}
+if (5 < 10) {
+	return true;
+} else {
+	return false;
+}
 
-		10 == 10;
-		10 != 9;
-		"foobar"
-		"foo bar"
-		`
+10 == 10;
+10 != 9;
+"foobar"
+"foo bar"
+`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -81,6 +81,31 @@ func TestNextToken(t *testing.T) {
 		{token.INT, "10"},
 		{token.GT, ">"},
 		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
 		{token.SEMICOLON, ";"},
 		{token.STRING, "foobar"},
 		{token.STRING, "foo bar"},
